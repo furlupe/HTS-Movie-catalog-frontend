7397b144f9dcce05dbd3fd4b727168b26019fd99
furlupe <101555696+furlupe@users.noreply.github.com>
2022-10-16 16:12:57 +0700

    Добавил оба вида навбара, и католог

---
+++ moviescatalog.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    post("https://react-midterm.kreosoft.space/api/account/login", {
+            username: "furlupe",
+            password: "strong"
+        })
+    .then(token => {
+        localStorage.setItem("userToken", token);
+    })
+    .catch(reason => console.log(reason))
+
+    showMovies();
+});
+
+function showMovies(){
+    get("https://react-midterm.kreosoft.space/api/movies/1")
+    .then(res => {
+        $("#movies").empty()
+        $template = $(".movie-template");
+        for (var movie of res.movies) {
+            $movieCard = $template.clone(true, true);
+            $movieCard.removeClass("d-none");
+            $movieCard.attr("id", `movie-${movie.id}`);
+            $movieCard.find(".movie-poster").attr("src", movie.poster)
+            $movieCard.find(".movie-name").text(movie.name);
+            $movieCard.find(".movie-year").text(movie.year);
+            $movieCard.find(".movie-extinfo").text(
+                `${movie.country} • ${movie.genres.map((item) => { return item.name }).join(", ")}`
+            ) // из json'а выдернуть все названия жанров, объединить в список и соединить через ,
+            $movieCard.find(".movie-rating").text(`Средняя оценка: ${countAvgRating(movie)}`)
+            $("#movies").append($movieCard);
+        }
+
+        registerPressMovieEvents();
+    }).catch(error => console.log(error));
+}
+
+// шаблон для функции перехода на страницу фильма
+function registerPressMovieEvents() {
+    $(".movie-template").click(function() {
+        console.log($(this).find(".movie-name").text())
+    })
+}
+
+function countAvgRating(movie) {
+    return ((movie.reviews.length < 1) ? 0 : // если рецензий нет, вернуть 0 как среднюю оценку
+    movie.reviews.map((review) => { 
+        return review.rating 
+    })
+    .reduce((a, b) => {
+        return a + b
+    }, 0) / movie.reviews.length).toFixed(1);
+}
---
+++ nav.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $.get("nav.html", function(data) {
+        $("#nav-placeholder").replaceWith(data);
+        loadNavbar();
+    });
+})
+
+function loadNavbar() {
+    var token = localStorage.getItem("userToken");
+    get("https://react-midterm.kreosoft.space/api/account/profile", token)
+    .then(json => {
+        $(".left-side").find("#favorites").removeClass('d-none');
+        $(".left-side").find("#profile").removeClass('d-none');
+        
+        $(".right-side").find("#login").addClass('d-none');
+        $(".right-side").find("#signup").addClass('d-none');
+
+        $(".right-side").find("#authas").removeClass('d-none');
+        $(".right-side").find("#nickname").removeClass('d-none').text(json.nickName);
+        $(".right-side").find("#signout").removeClass('d-none');
+    }, 
+    () => {
+        $(".right-side").find("#nickname").addClass('d-none');
+        $(".right-side").find("#signout").addClass('d-none');
+        $(".right-side").find("#authas").addClass('d-none');
+
+        $(".right-side").find("#login").removeClass('d-none');
+        $(".right-side").find("#signup").removeClass('d-none');
+
+        $(".left-side").find("#favorites").addClass('d-none');
+        $(".left-side").find("#profile").addClass('d-none');
+    })
+}
