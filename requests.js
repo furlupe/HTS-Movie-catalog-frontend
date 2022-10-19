d6c5675a456ebef361e4f6fb3d0ea564b3752d04
furlupe <101555696+furlupe@users.noreply.github.com>
2022-10-19 13:22:56 +0700

    added requset.js file

---
+++ requests.js
@@ -0,0 +1,26 @@
+function post(url, body) {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+    .then(response => response.json())
+    .catch(reason => console.log(reason))
+    .then(user => {
+        return user.token;
+    })
+}
+
+function get(url, token) {
+    return fetch(url, {
+        headers: new Headers({
+            "Authorization": "Bearer " + token
+        })
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(reason => console.log(reason))
+}
