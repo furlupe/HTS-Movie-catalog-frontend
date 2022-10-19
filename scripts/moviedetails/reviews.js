63f6ab3184927aa4efb53b27acdfc8a5e1cf4d65
furlupe <101555696+furlupe@users.noreply.github.com>
2022-10-19 18:58:14 +0700

    Добавил отображение отзывов других юзеров

--- scripts/moviedetails/reviews.js
+++ scripts/moviedetails/reviews.js
@@ -1,3 +1,24 @@
 function showReviews(reviews) {
-    
+    $template = $(".review-template")
+    for(var review of reviews) {
+        $r = $template.clone();
+        $r.removeClass("d-none");
+
+        var avatar = review.author.avatar;
+        var color = (review.rating > 5) ? "success" : "danger";
+
+        $r.addClass(`border-${color}`);
+
+        $r.find(".user-avatar").attr("src", (avatar) ? avatar : "");
+        $r.find(".user-name").text(review.author.nickName);
+        $r.find(".review-date").text(review.createDateTime);
+
+        $r.find(".review-rating").text(review.rating)
+        .addClass(`bg-${color}`);
+
+        $r.find(".review-text").text(review.reviewText)
+        .addClass(`text-${color}`);
+
+        $("#reviews").append($r);
+    }
 }
--- scripts/moviedetails/show_details_reviews.js
+++ scripts/moviedetails/show_details_reviews.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     get(`https://react-midterm.kreosoft.space/api/movies/details/${id}`)
     .then(details => {
         showMovieDetails(details);
+        console.log(details.reviews);
         showReviews(details.reviews);
     })
     .catch(error => console.log(error));
