66809e63bbda2cda6908f92a1fc6d70c13681905
furlupe <101555696+furlupe@users.noreply.github.com>
2022-11-02 18:28:30 +0700

    Добавил ограничение на рейтинг отзыва При больше 10 урезается до 10 При меньше 0 поднимается до 0

--- scripts/moviedetails/review_form.js
+++ scripts/moviedetails/review_form.js
@@ -2,23 +2,27 @@ import {post, put} from "./../requests.js"
 import { URL_ADD_REVIEW, URL_EDIT_REVIEW} from "../requests_consts.js";
 
 export function registerReviewFormEvents(movieId, id) {
+    let rating = parseInt($(".user-review-form #reviewRating").val())
+    rating = rating < 10 ? (rating > 0 ? rating : 0) : 10;
+
     $(".save-review-button").click(() => {
         post(URL_ADD_REVIEW(movieId), {
             "reviewText": $(".user-review-form #reviewText").val(),
-            "rating": parseInt($(".user-review-form #reviewRating").val()),
+            "rating": rating,
             "isAnonymous": $(".user-review-form #reviewAnon").is(':checked')
         })
         .then(() => location.reload());
     })
 
     $(".edit-review-button").click(() => {
+        let rating = parseInt($(".user-review-form #reviewRating").val())
         put(URL_EDIT_REVIEW(movieId, id), {
             "reviewText": $(".user-review-form #reviewText").val(),
-            "rating": parseInt($(".user-review-form #reviewRating").val()),
+            "rating": rating,
             "isAnonymous": $(".user-review-form #reviewAnon").is(':checked')
         })
         .then(() => {
-            location.reload()
+            //location.reload()
         });
     })
 
