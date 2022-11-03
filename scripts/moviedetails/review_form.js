eda7213a15d3a755b420dd96e76d1dad60835410
Volgin Ilia <govnihorse@gmail.com>
2022-11-03 10:59:10 +0700

    Изменена проверка оценки на входимость в [0; 10]

--- scripts/moviedetails/review_form.js
+++ scripts/moviedetails/review_form.js
@@ -2,13 +2,11 @@ import {post, put} from "./../requests.js"
 import { URL_ADD_REVIEW, URL_EDIT_REVIEW} from "../requests_consts.js";
 
 export function registerReviewFormEvents(movieId, id) {
-    let rating = parseInt($(".user-review-form #reviewRating").val())
-    rating = rating < 10 ? (rating > 0 ? rating : 0) : 10;
 
     $(".save-review-button").click(() => {
         post(URL_ADD_REVIEW(movieId), {
             "reviewText": $(".user-review-form #reviewText").val(),
-            "rating": rating,
+            "rating": checkRating(parseInt($(".user-review-form #reviewRating").val())),
             "isAnonymous": $(".user-review-form #reviewAnon").is(':checked')
         })
         .then(() => location.reload());
@@ -18,7 +16,7 @@ export function registerReviewFormEvents(movieId, id) {
         let rating = parseInt($(".user-review-form #reviewRating").val())
         put(URL_EDIT_REVIEW(movieId, id), {
             "reviewText": $(".user-review-form #reviewText").val(),
-            "rating": rating,
+            "rating": checkRating(parseInt($(".user-review-form #reviewRating").val())),
             "isAnonymous": $(".user-review-form #reviewAnon").is(':checked')
         })
         .then(() => {
@@ -29,4 +27,9 @@ export function registerReviewFormEvents(movieId, id) {
     $(".cancel-edit-review-button").click(() => {
         location.reload();
     })
+}
+
+function checkRating(rating) {
+    return rating = rating < 10 ? 
+    (rating > 0 ? rating : 0) : 10;
 }
